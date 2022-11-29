d0e745a7a506c93a274e094c5b8f5794b5360c6a
captbay <agespramana9@gmail.com>
2022-11-29 14:12:10 +0700

    Initial commit testing

---
+++ app/src/main/java/com/example/gd13_a_0994/models/Mahasiswa.kt
@@ -0,0 +1,5 @@
+package com.example.gd13_a_0994.models
+
+class Mahasiswa(var nama: String, var npm: String, var fakultas: String, var prodi: String) {
+    var id: Long? = null
+}
---
+++ app/src/main/java/com/example/gd13_a_0994/models/MahasiswaResponse.kt
@@ -0,0 +1,5 @@
+package com.example.gd13_a_0994.models
+
+class MahasiswaResponse {
+//kalo pake modelan messagenya di luar data (kek PW) jadi harus dipisah saat ambil
+}
